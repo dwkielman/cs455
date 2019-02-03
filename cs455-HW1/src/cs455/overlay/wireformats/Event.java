7cd3977c9b50cbbad5d18cd8c650e2dcfe187b12
dwkielman <dwkielman@gmail.com>
2019-02-02 19:42:54 -0800

    Wasn't commited before for some reason

--- cs455-HW1/src/cs455/overlay/wireformats/Event.java
+++ cs455-HW1/src/cs455/overlay/wireformats/Event.java
@@ -2,6 +2,10 @@ package cs455.overlay.wireformats;
 
 import java.io.IOException;
 
+/**
+ * Event interface with the getType() and getBytes() defined
+ */
+
 public interface Event {
 	
 	public int getType();
