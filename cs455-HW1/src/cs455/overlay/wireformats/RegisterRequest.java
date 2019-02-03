ad424d3d4f4c8c6a66fd2458418d329df78d00f7
dwkielman <dwkielman@gmail.com>
2019-02-02 16:29:46 -0800

    RegistryRequest Written

---
+++ cs455-HW1/src/cs455/overlay/wireformats/RegisterRequest.java
@@ -0,0 +1,87 @@
+package cs455.overlay.wireformats;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Used for nodes requesting registering with the registry
+ * Message Type (int): REGISTER_REQUEST (6000)
+ * IP address (String)
+ * Port number (int)
+ */
+public class RegisterRequest implements Event {
+
+	private final int type = Protocol.REGISTER_REQUEST;
+	private String IPAddress;
+	private int portNumber;
+	
+	public RegisterRequest(String IPAddress, int portNumber) {
+		this.IPAddress = IPAddress;
+		this.portNumber = portNumber;
+	}
+	
+	/**
+	 * byte[] construction is as follows:
+	 * type
+	 * IPAddress
+	 * portNumber
+	 * @throws IOException 
+	 */
+	public RegisterRequest(byte[] marshalledBytes) throws IOException {
+		ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+		DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+		
+		int type = din.readInt();
+		
+		if (type != Protocol.REGISTER_REQUEST) {
+			System.out.println("Invalid Message Type for RegisterRequest");
+			return;
+		}
+		
+		int IPAddressLength = din.readInt();
+		byte[] IPAddressBytes = new byte[IPAddressLength];
+		din.readFully(IPAddressBytes);
+		
+		this.IPAddress = new String(IPAddressBytes);
+		
+		int portNumber = din.readInt();
+
+		this.portNumber = portNumber;
+		
+		baInputStream.close();
+		din.close();
+	}
+
+	@Override
+	public int getType() {
+		return this.type;
+	}
+
+	@Override
+	public byte[] getBytes() throws IOException {
+		byte[] marshalledBytes = null;
+		ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+		DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+		dout.writeInt(this.type);
+		
+		byte[] IPAddressBytes = this.IPAddress.getBytes();
+		int IPAddressLength = IPAddressBytes.length;
+		dout.writeInt(IPAddressLength);
+		dout.write(IPAddressBytes);
+		
+		dout.writeInt(this.portNumber);
+		
+		dout.flush();
+		marshalledBytes = baOutputStream.toByteArray();
+		baOutputStream.close();
+		dout.close();
+		
+		return marshalledBytes;
+	}
+
+}
