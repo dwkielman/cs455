30cada4452fb3dd7d9d7a5f71ccd90f909d7bc85
dwkielman <dwkielman@gmail.com>
2019-02-02 20:20:55 -0800

    TaskSummaryResponse and Message messages

--- cs455-HW1/src/cs455/overlay/wireformats/Message.java
+++ cs455-HW1/src/cs455/overlay/wireformats/Message.java
@@ -1,5 +1,125 @@
 package cs455.overlay.wireformats;
 
-public class Message {
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Data can be fed into the network from any messaging node within the network. Packets are sent from a source to a sink.
+ * A message includes a payload which is a random integer.
+ * Message Type (int): MESSAGE (6010)
+ * Source IP address (String)
+ * Source Port number (int)
+ * Destination IP address (String)
+ * Destination Port number (int)
+ * Payload (int)
+ */
+
+public class Message implements Event {
+
+	private final int type = Protocol.MESSAGE;
+	private String sourceIPAddress;
+	private int sourcePortNumber;
+	private String destiantionIPAddress;
+	private int destiantionPortNumber;
+	private int payload;
+	
+	public Message(String sourceIPAddress, int sourcePortNumber, String destiantionIPAddress, int destiantionPortNumber, int payload) {
+		this.sourceIPAddress = sourceIPAddress;
+		this.sourcePortNumber = sourcePortNumber;
+		this.destiantionIPAddress = destiantionIPAddress;
+		this.destiantionPortNumber = destiantionPortNumber;
+		this.payload = payload;
+	}
+
+	/**
+	 * byte[] construction is as follows:
+	 * type
+	 * sourceIPAddress
+	 * sourcePortNumber
+	 * destiantionIPAddress
+	 * destiantionPortNumber
+	 * payload
+	 * @throws IOException 
+	 */
+	public Message(byte[] marshalledBytes) throws IOException {
+		ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+		DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+		
+		int type = din.readInt();
+		
+		if (type != Protocol.REGISTER_REQUEST) {
+			System.out.println("Invalid Message Type for RegisterRequest");
+			return;
+		}
+		
+		int sourceIPAddressLength = din.readInt();
+		byte[] sourceIPAddressBytes = new byte[sourceIPAddressLength];
+		din.readFully(sourceIPAddressBytes);
+		
+		this.sourceIPAddress = new String(sourceIPAddressBytes);
+		
+		int sourcePortNumber = din.readInt();
+
+		this.sourcePortNumber = sourcePortNumber;
+		
+		int destiantionIPAddressLength = din.readInt();
+		byte[] destiantionIPAddressBytes = new byte[destiantionIPAddressLength];
+		din.readFully(destiantionIPAddressBytes);
+		
+		this.destiantionIPAddress = new String(destiantionIPAddressBytes);
+		
+		int destiantionPortNumber = din.readInt();
+
+		this.destiantionPortNumber = destiantionPortNumber;
+		
+		int payload = din.readInt();
+
+		this.payload = payload;
+		
+		baInputStream.close();
+		din.close();
+	}
+
+	@Override
+	public int getType() {
+		return this.type;
+	}
+
+	@Override
+	public byte[] getBytes() throws IOException {
+		byte[] marshalledBytes = null;
+		ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+		DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+		dout.writeInt(this.type);
+		
+		byte[] sourceIPAddressBytes = this.sourceIPAddress.getBytes();
+		int sourceIPAddressLength = sourceIPAddressBytes.length;
+		dout.writeInt(sourceIPAddressLength);
+		dout.write(sourceIPAddressBytes);
+		
+		dout.writeInt(this.sourcePortNumber);
+		
+		byte[] destiantionIPAddressBytes = this.destiantionIPAddress.getBytes();
+		int destiantionIPAddressLength = destiantionIPAddressBytes.length;
+		dout.writeInt(destiantionIPAddressLength);
+		dout.write(destiantionIPAddressBytes);
+		
+		dout.writeInt(this.destiantionPortNumber);
+		
+		dout.writeInt(this.payload);
+		
+		dout.flush();
+		marshalledBytes = baOutputStream.toByteArray();
+		baOutputStream.close();
+		dout.close();
+		
+		return marshalledBytes;
+	}
+
 
 }
--- cs455-HW1/src/cs455/overlay/wireformats/TaskSummaryResponse.java
+++ cs455-HW1/src/cs455/overlay/wireformats/TaskSummaryResponse.java
@@ -1,35 +1,135 @@
 package cs455.overlay.wireformats;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
- * Once the registry has received TASK_COMPLETE messages from all the registered nodes it will issue a PULL_TRAFFIC_SUMMARY message. This message is sent to all the registered nodes in the system.
- * Message Type: TRAFFIC_SUMMARY
- * Node IP address:
- * Node Port number:
- * Number of messages sent
- * Summation of sent messages
- * Number of messages received
- * Summation of received messages
- * Number of messages relayed
+ * Upon receipt of the PULL_TRAFFIC_SUMMARY message from the registry, the node will create a response that includes summaries of the traffic that it has participated in. The summary will include information about messages that were sent and received.
+ * Message Type: TRAFFIC_SUMMARY (6009)
+ * Node IP address (String):
+ * Node Port number (int):
+ * Number of messages sent (int)
+ * Summation of sent messages (long)
+ * Number of messages received (int)
+ * Summation of received messages (long)
+ * Number of messages relayed (int)
  */
 
 public class TaskSummaryResponse implements Event {
 
-	public TaskSummaryResponse(byte[] marshalledBytes) {
-		// TODO Auto-generated constructor stub
+	private final int type = Protocol.TRAFFIC_SUMMARY;
+	private String IPAddress;
+	private int portNumber;
+	private int numberOfMessagesSent;
+	private long sumSentMessages;
+	private int numberOfMessagesReceived;
+	private long sumReceivedMessages;
+	private int numberOfMessagesRelayed;
+	
+	
+	
+	public TaskSummaryResponse(String iPAddress, int portNumber, int numberOfMessagesSent, long sumSentMessages, int numberOfMessagesReceived, long sumReceivedMessages, int numberOfMessagesRelayed) {
+		this.IPAddress = iPAddress;
+		this.portNumber = portNumber;
+		this.numberOfMessagesSent = numberOfMessagesSent;
+		this.sumSentMessages = sumSentMessages;
+		this.numberOfMessagesReceived = numberOfMessagesReceived;
+		this.sumReceivedMessages = sumReceivedMessages;
+		this.numberOfMessagesRelayed = numberOfMessagesRelayed;
+	}
+
+	/**
+	 * byte[] construction is as follows:
+	 * type
+	 * IPAddress
+	 * portNumber
+	 * numberOfMessagesSent
+	 * sumSentMessages
+	 * numberOfMessagesReceived
+	 * sumReceivedMessages
+	 * numberOfMessagesRelayed
+	 * @throws IOException 
+	 */
+	public TaskSummaryResponse(byte[] marshalledBytes) throws IOException {
+		ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+		DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+		
+		int type = din.readInt();
+		
+		if (type != Protocol.TRAFFIC_SUMMARY) {
+			System.out.println("Invalid Message Type for TaskSummaryResponse");
+			return;
+		}
+		
+		int IPAddressLength = din.readInt();
+		byte[] IPAddressBytes = new byte[IPAddressLength];
+		din.readFully(IPAddressBytes);
+		
+		this.IPAddress = new String(IPAddressBytes);
+		
+		int portNumber = din.readInt();
+
+		this.portNumber = portNumber;
+		
+		int numberOfMessagesSent = din.readInt();
+		
+		this.numberOfMessagesSent = numberOfMessagesSent;
+		
+		long sumSentMessages = din.readLong();
+		
+		this.sumSentMessages = sumSentMessages;
+		
+		int numberOfMessagesReceived = din.readInt();
+		
+		this.numberOfMessagesReceived = numberOfMessagesReceived;
+		
+		long sumReceivedMessages = din.readLong();
+		
+		this.sumReceivedMessages = sumReceivedMessages;
+		
+		int numberOfMessagesRelayed = din.readInt();
+		
+		this.numberOfMessagesRelayed = numberOfMessagesRelayed;
+		
+		baInputStream.close();
+		din.close();
 	}
 
 	@Override
 	public int getType() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.type;
 	}
 
 	@Override
 	public byte[] getBytes() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		byte[] marshalledBytes = null;
+		ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+		DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+		dout.writeInt(this.type);
+		
+		byte[] IPAddressBytes = this.IPAddress.getBytes();
+		int IPAddressLength = IPAddressBytes.length;
+		dout.writeInt(IPAddressLength);
+		dout.write(IPAddressBytes);
+		
+		dout.writeInt(this.portNumber);
+		dout.writeInt(this.numberOfMessagesSent);
+		dout.writeLong(this.sumSentMessages);
+		dout.writeInt(this.numberOfMessagesReceived);
+		dout.writeLong(this.sumReceivedMessages);
+		dout.writeInt(this.numberOfMessagesRelayed);
+		
+		dout.flush();
+		marshalledBytes = baOutputStream.toByteArray();
+		baOutputStream.close();
+		dout.close();
+		
+		return marshalledBytes;
 	}
 
 }
