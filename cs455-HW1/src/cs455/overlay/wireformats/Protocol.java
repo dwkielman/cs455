0c1d9df1fad4daf5561c96cafc517292fa69e004
dwkielman <dwkielman@gmail.com>
2019-02-02 18:34:16 -0800

    Current state of Protocol Types

--- cs455-HW1/src/cs455/overlay/wireformats/Protocol.java
+++ cs455-HW1/src/cs455/overlay/wireformats/Protocol.java
@@ -1,5 +1,19 @@
 package cs455.overlay.wireformats;
 
-public class Protocol {
+/**
+ * Interactions between the registry and the messaging nodes, including prescribed wire-formats
+ */
 
+public class Protocol {
+	public static final int REGISTER_REQUEST = 6000;
+	public static final int REGISTER_RESPONSE = 6001;
+	public static final int DEREGISTER_REQUEST = 6002;
+	public static final int DEREGISTER_RESPONSE = 6003;
+	public static final int MESSAGING_NODES_LIST = 6004;
+	public static final int LINK_WEIGHTS = 6005;
+	public static final int TASK_INITIATE = 6006;
+	public static final int TASK_COMPLETE = 6007;
+	public static final int PULL_TRAFFIC_SUMMARY = 6008;
+	public static final int TRAFFIC_SUMMARY = 6009;
+	public static final int MESSAGE = 6010;
 }
