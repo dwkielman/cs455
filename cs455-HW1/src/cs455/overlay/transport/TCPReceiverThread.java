b91fb836d5e4c64e97d2cec403739c24affddf0d
dwkielman <dwkielman@gmail.com>
2019-02-03 17:36:36 -0800

    First attempt at Receiver and Server Threads.

--- cs455-HW1/src/cs455/overlay/transport/TCPReceiverThread.java
+++ cs455-HW1/src/cs455/overlay/transport/TCPReceiverThread.java
@@ -5,36 +5,52 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
 
-public class TCPReceiverThread {
+import cs455.overlay.node.Node;
+import cs455.overlay.wireformats.Event;
+import cs455.overlay.wireformats.EventFactory;
 
-	public class TCPReceiver implements Runnable {
+public class TCPReceiverThread implements Runnable {
+
+	private Socket socket;
+	private DataInputStream din;
+	private Node node;
+	private EventFactory eventFactory;
+	private static final boolean DEBUG = true;
 		
-		private Socket socket;
-		private DataInputStream din;
+	public TCPReceiverThread(Socket socket, Node node) throws IOException {
+		this.node = node;
+		this.socket = socket;
+		this.eventFactory = EventFactory.getInstance();
+		din = new DataInputStream(socket.getInputStream());
+	}
 		
-		public TCPReceiver(Socket socket) throws IOException {
-			this.socket = socket;
-			din = new DataInputStream(socket.getInputStream());
-		}
+	@Override
+	public void run() {
 		
-		@Override
-		public void run() {
-			
-			int dataLength;
-			
-			while (socket != null) {
-				try {
-					dataLength = din.readInt();
-					byte[] data = new byte[dataLength];
-					din.readFully(data, 0, dataLength);
-				} catch (SocketException se) {
-					System.out.println(se.getMessage());
-					break;
-				} catch (IOException ioe) {
-					System.out.println(ioe.getMessage());
-					break;
-				}
+		int dataLength;
+		
+		while (socket != null) {
+			try {
+				if (DEBUG) System.out.println("TCPReceiverThread waiting for message length...");
+				dataLength = din.readInt();
+				if (DEBUG) System.out.println("TCPReceiverThread received message of length: " + dataLength);
+				if (DEBUG) System.out.println("TCPReceiverThread awaiting byte array message delivery...");
+				byte[] data = new byte[dataLength];
+				din.readFully(data, 0, dataLength);
+				String str = new String(data, "UTF-8");
+				if (DEBUG) System.out.println("TCPReceiverThread received byte[]: " + str);
+				
+				// Notify node of event
+				eventFactory.createEvent(data, this.node);
+				
+			} catch (SocketException se) {
+				System.out.println(se.getMessage());
+				break;
+			} catch (IOException ioe) {
+				System.out.println(ioe.getMessage());
+				break;
 			}
 		}
+		if (DEBUG) System.out.println("  TCPReceiverThread exiting.");
 	}
 }
--- cs455-HW1/src/cs455/overlay/transport/TCPServerThread.java
+++ cs455-HW1/src/cs455/overlay/transport/TCPServerThread.java
@@ -1,5 +1,66 @@
 package cs455.overlay.transport;
 
-public class TCPServerThread {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import cs455.overlay.node.Node;
+
+public class TCPServerThread extends Thread {
+	
+	private static final boolean DEBUG = true;
+	private Node node;
+	private ServerSocket ourServerSocket;
+	private String hostIPAddress;
+	private int portNumber;
+	
+	
+	public TCPServerThread(int portNumber, Node node) {
+		try {
+			//Create the server socket
+			ourServerSocket = new ServerSocket(portNumber);
+			this.portNumber = ourServerSocket.getLocalPort();
+			this.node = node;
+			this.node.setLocalHostPortNumber(ourServerSocket.getLocalPort());
+		} catch(IOException e) {
+			System.out.println("TCPServerThread::creating_the_socket:: " + e);
+			System.exit(1);
+		}
+	}
+	
+	public void run() {
+		
+		// get the current host IP address
+		try {
+			this.hostIPAddress = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException uhe) {
+			uhe.printStackTrace();
+		}
+		
+		// assign the port to the actual port number
+		//this.portNumber = ourServerSocket.getLocalPort();
+		
+		if (DEBUG) {
+			System.out.println("Node is now listening on IP: " + this.hostIPAddress + " Port: " + this.portNumber);
+		}
+		
+		while (true) {
+			try {
+				//Block on accepting connections. Once it has received a connection it will return a socket for us to use.
+				Socket incomingConnectionSocket = ourServerSocket.accept();
+				TCPReceiverThread tcpReceiverThread = new TCPReceiverThread(incomingConnectionSocket, this.node);
+				tcpReceiverThread.run();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("TCPServerThread::accepting_connections:: " + e);
+	            System.exit(1);
+			}
+			
+		}
+	}
 
 }
